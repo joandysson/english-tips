eec2c0ed43d668213afee1e0c0111dffa9934829
joandysson <joandysson.gama@gmail.com>
2025-07-06 16:57:51 -0300

    CSS

--- public/views/templates/main.php
+++ public/views/templates/main.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo asset('css/main.css?v=0.8') ?>">
+    <link rel="stylesheet" href="<?php echo asset('css/main.css?v=0.9') ?>">
 
     <?php
     if (isset($headExtra)) {
